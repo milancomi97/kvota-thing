4a24915586450bdbc8af6f652b427cfaf0c4d7b5
milancomi97 <133712487+milancomi97@users.noreply.github.com>
2026-01-11 18:53:18 +0100

    authenticate greska

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,13 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if ($request->expectsJson()) {
+            return null;
+        }
+
+        // site dolazi iz ResolveSite middleware-a
+        $site = app('site');
+
+        return route('login', ['site' => $site]);
     }
 }
